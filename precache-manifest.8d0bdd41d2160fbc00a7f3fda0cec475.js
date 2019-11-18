3580509c51be1bd01777f535ff2752ff378888db
SiddharthPandey27 <siddharthpandeyit3090@gmail.com>
2019-11-18 13:08:38 +0530

    Updates

---
+++ precache-manifest.8d0bdd41d2160fbc00a7f3fda0cec475.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6586a526e18f201d329c1a33861e034",
+    "url": "/monster-class-room/index.html"
+  },
+  {
+    "revision": "7f7a792159dff410d7f7",
+    "url": "/monster-class-room/static/css/main.0d738cf4.chunk.css"
+  },
+  {
+    "revision": "119e9f86b94a1ad19451",
+    "url": "/monster-class-room/static/js/2.e008a836.chunk.js"
+  },
+  {
+    "revision": "7f7a792159dff410d7f7",
+    "url": "/monster-class-room/static/js/main.130fe92e.chunk.js"
+  },
+  {
+    "revision": "be66e339bd2551ecbeac",
+    "url": "/monster-class-room/static/js/runtime-main.fe049e93.js"
+  }
+]);
